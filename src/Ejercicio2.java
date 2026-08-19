ba915bec0e5edfe821b65da1f6a1fcd8ebabdf3f
juliana parra <juliparra481@gmail.com>
2026-08-19 18:02:50 -0500

    hice el segundo ejercicio

---
+++ src/Ejercicio2.java
@@ -0,0 +1,9 @@
+public class Ejercicio2 {
+    public static void main(String[] args){
+        saludarEstudiante("juliana", 16);
+
+    }
+    public static void  saludarEstudiante (String nombre, int edad){
+        System.out.println("Hola " + nombre + ", tienes " + edad + " años.");
+    }
+}
