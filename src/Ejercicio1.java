8f707bcdda8827c36bc51f2c640655103ac4a9c3
juliana parra <juliparra481@gmail.com>
2026-08-19 17:46:10 -0500

    hice el primer ejercicio

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,2 +1,10 @@
 public class Ejercicio1 {
-}
+        public static void main(String[] args) {
+            mostrarBienvenida();
+
+        }
+        public static void mostrarBienvenida() {
+            System.out.println("Bienvenido al instituto");
+        }
+    }
+
