004e2fbb40b1f6aa2abba4c21f8dd6b8907740b8
juliana parra <juliparra481@gmail.com>
2026-08-19 20:38:19 -0500

    hice el septimo ejercicio

---
+++ src/Ejercicio7.java
@@ -0,0 +1,18 @@
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        double[] calificaciones = {4.0, 3.5, 5.0, 4.5};
+        double promedio = calcularPromedioClase(calificaciones);
+        System.out.println("El promedio de la clase es: " + promedio);
+    }
+
+    public static double calcularPromedioClase(double[] calificaciones) {
+        double suma = 0;
+        for (int i = 0; i < calificaciones.length; i++) {
+            suma = suma + calificaciones[i];
+        }
+        return suma / calificaciones.length;
+    }
+}
+
+
